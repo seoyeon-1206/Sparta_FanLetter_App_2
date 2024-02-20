857e33f802b9d58a84cfb5d294cda84e54519896
seoyeon-1206 <ssy_1206@naver.com>
2024-02-20 12:52:11 +0900

    [feature] 로그인, 회원가입 토글 구현

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,23 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { useSearchParams } from 'react-router-dom'
 import styled from 'styled-components'
 
 const Login = () => {
+    const [isLoginForm, setIsLoginForm] = useState(true);
+
+    const toggleForm = () => {
+        setIsLoginForm(!isLoginForm)
+    }
     return (
         <>
             <LoginWrapper>
                 <LoginForm>
-                    <PageTitle>로그인</PageTitle>
+                    <PageTitle>{isLoginForm ? '로그인' : '회원가입'}</PageTitle>
                     <InputWrapper>
                         <input name='id' placeholder='아이디 (4~10글자)' minLength="4" maxLength="10"></input>
                     </InputWrapper>
                     <InputWrapper>
                         <input name='password' placeholder='비밀번호 (4~15글자)' minLength="4" maxLength="15"></input>
                     </InputWrapper>
+                    {!isLoginForm && (
+                        <InputWrapper>
+                            <input name='nickname' placeholder='닉네임 (1~10글자)' minLength="1" maxLength="10"></input>
+                        </InputWrapper>
+                    )}
                     <TopBtn>
-                        <button>로그인</button>
+                        <button>{isLoginForm ? '로그인' : '회원가입'} </button>
                     </TopBtn>
                     <BottomBtn>
-                        <span>회원가입</span>
+                        <span onClick={toggleForm}>{isLoginForm ? '회원가입' : '로그인'}</span>
                     </BottomBtn>
                 </LoginForm>
             </LoginWrapper>
@@ -27,14 +38,14 @@ const Login = () => {
 
 export default Login
 
-export const LoginWrapper = styled.div`
+const LoginWrapper = styled.div`
     display: flex;
     justify-content: center;
     align-items: center;
     height: 100vh;
     background-color: lightgray;
 `
-export const LoginForm = styled.form`
+const LoginForm = styled.form`
     display: flex;
     flex-direction: column;
     padding: 12px 18px;
@@ -42,12 +53,12 @@ export const LoginForm = styled.form`
     background-color: white;
     width: 500px;
 `
-export const PageTitle = styled.h1`
+const PageTitle = styled.h1`
 font-size: 36px;
     margin-bottom: 36px;
     
 `
-export const InputWrapper = styled.div`
+const InputWrapper = styled.div`
     margin-bottom: 24px;
     padding: 12px 0px;
     border-top: none;
@@ -63,7 +74,7 @@ export const InputWrapper = styled.div`
     outline: none;
     }
 `
-export const TopBtn = styled.div`
+const TopBtn = styled.div`
     display: flex;
     justify-content: flex-end;
     align-items: center;
@@ -78,7 +89,7 @@ export const TopBtn = styled.div`
     border: none;
     }
 `
-export const BottomBtn = styled.form`
+const BottomBtn = styled.form`
 text-align: center;
     font-size: 16px;
     color: lightgray;
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -2,14 +2,13 @@ import Detail from "pages/Detail";
 import Home from "pages/Home";
 import Login from "pages/Login";
 import Profile from "pages/Profile";
-import SignUp from "pages/SignUp";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<SignUp />} />
+        <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
         <Route path="/profile" element={<Profile />} />
