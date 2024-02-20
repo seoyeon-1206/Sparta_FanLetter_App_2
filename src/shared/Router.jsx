faa1b8c32cce48c7783cac5481230b229e4212f7
seoyeon-1206 <ssy_1206@naver.com>
2024-02-20 11:35:38 +0900

    [feature] router 셋업

--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,13 +1,17 @@
 import Detail from "pages/Detail";
 import Home from "pages/Home";
+import Login from "pages/Login";
+import Profile from "pages/Profile";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 export default function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/detail/:id" element={<Detail />} />
+        <Route path="/profile" element={<Profile />} />
         <Route path="*" element={<Navigate replace to="/" />} />
       </Routes>
     </BrowserRouter>
