aa2182faf178a2e4c8e3e64af5633fe3ce28f098
seoyeon-1206 <ssy_1206@naver.com>
2024-02-22 22:32:55 +0900

    [style] profile, 수정 취소 토글

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,67 @@
-import React from 'react'
+import Button from 'components/common/Button'
+import Avatar from 'components/common/Avatar'
+import React, { useState } from 'react'
+import styled from 'styled-components'
+import { useSelector } from 'react-redux'
 
 const Profile = () => {
+    const { avatar, nickname, userId } = useSelector(state => state.auth);
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [editingText, setEditingText] = useState("");
+
+
     return (
-        <div>Profile</div>
+        <Container>
+            <ProfileWrapper>
+                <h1>프로필 관리</h1>
+                <Avatar size="large" src={avatar} />
+                <Nickname>{nickname}</Nickname>
+                <UserId>{userId}</UserId>
+                {isEditing ? (<div>
+                    <Button text='취소' onClick={() => setIsEditing(false)} />
+                    <Button text='수정완료' />
+                </div>
+                ) : (
+                    <Button text='수정하기' onClick={() => setIsEditing(true)} />
+                )}
+            </ProfileWrapper>
+        </Container>
     )
 }
 
 export default Profile
+
+const Container = styled.div`
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`
+const ProfileWrapper = styled.section`
+    width: 500px;
+    border-radius: 12px;
+    background-color: lightgray;
+    padding: 12px;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 24px;
+
+    & h1 {
+        font-size: 36px;
+        font-weight: 700;
+    }
+    & div{
+        display: flex;
+        gap: 24px;
+    }
+`
+const Nickname = styled.span`
+    font-size: 24px;
+    font-weight: 700;
+`
+const UserId = styled.span`
+    font-size: 16px;
+    color: gray;
+`
